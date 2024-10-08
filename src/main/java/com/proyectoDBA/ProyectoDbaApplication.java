a625e6c083a26c77ee6c90ed7a3b5b1702cfee3c
Fabiola <fabiolaodio777@hotmail.com>
2024-10-07 18:37:28 -0600

    primer commit

---
+++ src/main/java/com/proyectoDBA/ProyectoDbaApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoDBA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoDBA/proyectoDBA/ProyectoDbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoDBA.proyectoDBA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
